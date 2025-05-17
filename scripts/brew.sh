9efb3212d17c59af7e486dec316bb09092c6c2ad
Shaharyar Shakir <shakirshaharyar125@gmail.com>
2025-05-17 19:07:19 +0500

    scritp to install homebrew

---
+++ scripts/brew.sh
@@ -0,0 +1,11 @@
+if ! command -v brew &> /dev/null
+then 
+echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew is already installed"
+fi 
+
+# Add brew to PATH only if it's not already there
+grep -qxF 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' ~/.bashrc || \
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
